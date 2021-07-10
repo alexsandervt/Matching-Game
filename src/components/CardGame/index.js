71e3030c4dccfa644784309e171da7808a5a1441
Alexsander <alextelles.dev@gmail.com>
2021-07-10 19:49:24 -0300

    :tada: Create CardGame Component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,13 @@
+// const CardGame = function() {}
+// const CardGame = () => {};
+import "./styles.css";
+
+function CardGame() {
+  return `
+    <article class="card-game">
+      <img src="images/alura.png" alt="Logo da Alura">
+    </article>  
+  `;
+}
+
+export default CardGame;
